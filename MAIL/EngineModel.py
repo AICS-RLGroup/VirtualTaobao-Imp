b7e1857adb14d34707124762d7626b9686858698
amax <2227085431@qq.com>
2019-12-04 19:41:49 +0800

    mail

---
+++ MAIL/EngineModel.py
@@ -0,0 +1,16 @@
+from utils.utils import *
+
+
+class EngineModel(nn.Module):
+    def __init__(self, n_input=88, n_hidden=256, n_output=27, activation=nn.LeakyReLU):
+        super(EngineModel, self).__init__()
+        self.model = nn.Sequential(
+            nn.Linear(n_input, n_hidden),
+            activation(),
+            nn.Linear(n_hidden, n_output),
+            nn.Sigmoid()
+        )
+        self.model.apply(init_weight)
+
+    def forward(self, x):
+        return self.model(x)
--- MAIL/MAIL.py
+++ MAIL/MAIL.py
@@ -1,6 +1,7 @@
 from MAIL.ActionModel import ActionModel
 from MAIL.LeaveModel import LeaveModel
 from MAIL.RewardModel import RewardModel
+from MAIL.EngineModel import EngineModel
 from GAN_SD.Gan_SD import GanSDModel
 from utils.utils import *
 
@@ -15,22 +16,27 @@ class MailModel():
         self.A = ActionModel()
         self.L = LeaveModel()
         self.R = RewardModel()
+        self.E = EngineModel()
+        self.G = GanSDModel()
+
+        self.loss_func = nn.BCELoss()
 
     def train(self):
         batch_num = (len(self.expert_data) + self.batch_size - 1) // self.batch_size
         for epoch in range(100):
+            trajectory = []
+            for j in range(self.trajectory_num):
+                tao_j = []
+                s,_ = self.G.generate()
+                a = self.E(s)
+                s_c = torch.cat((s,a),dim=1)
+                while True:
+
+                    pass
+
             idx = torch.randperm(len(self.expert_data))
             for i in range(batch_num):
                 batch_expert = self.expert_data[idx[i * self.batch_size:(i + 1) * self.batch_size]]
-                trajectory = []
-                for j in range(self.trajectory_num):
-                    tao_j = []
-                    s = GanSDModel()
-
-                    while True:
-
-                        pass
-
 
 
     def save_model(self):
