5234e1504fe2ca16f6070ff078356acdcddffdf9
RITCHIEHuang <ritchie445412@gmail.com>
2019-12-05 15:28:47 +0800

    MAIL Policy

--- MAIL/MAIL.py
+++ MAIL/MAIL.py
@@ -1,30 +1,23 @@
 import random
 
-from GAN_SD.GeneratorModel import GeneratorModel
-from MAIL.EnginePolicy import EnginePolicy
+from MAIL.MailPolicy import MailPolicy
 from MAIL.RewardModel import RewardModel
-from MAIL.UserLeaveModel import UserLeaveModel
-from MAIL.UserPolicy import UserPolicy
 from utils.utils import *
 
 
 class MailModel:
-    def __init__(self, expert_data, lr_r=0.0005, trajectory_num=10000, batch_size=32):
+    def __init__(self, expert_data, lr_d=0.0001, lr_g=0.0005, trajectory_num=10000, batch_size=32):
         self.expert_data = expert_data
 
         self.trajectory_num = trajectory_num
         self.batch_size = batch_size
 
-        self.UserModel = GeneratorModel()
-        self.UserModel.load()
+        self.G = RewardModel()
+        self.D = MailPolicy()
 
-        self.UserPolicy = UserPolicy()
-        self.UserLeaveModel = UserLeaveModel()
-        self.RewardModel = RewardModel()
+        self.optim_G = optim.Adam(self.G.parameters(), lr=lr_g)
+        self.optim_D = optim.Adam(self.D.parameters(), lr=lr_d)
 
-        self.EnginePolicy = EnginePolicy()
-
-        self.optim_R = optim.Adam(self.EnginePolicy.parameters(), lr=lr_r)
         self.loss_func = nn.BCELoss()
 
     def train(self):
@@ -33,30 +26,7 @@ class MailModel:
 
             for i in range(batch_num):
 
-                # sample J trajectories
-                trajectory = []
-                for j in range(self.trajectory_num):
-                    tao_j = []
-                    # sample user from GAN-SD distribution
-                    s, _ = self.UserModel.generate()
-                    # get user's leave page index from leave model
-                    leave_page_index = self.UserLeaveModel(s)
-                    # get engine action from user with request
-                    a = self.EnginePolicy(s)
-
-                    s_c = torch.cat((s, a), dim=1)
-                    page_index = 1
-
-                    while page_index != leave_page_index:  # terminate condition
-                        s_c = torch.cat((s_c, FLOAT(page_index)).to(device), dim=1)
-                        a_c = self.UserPolicy.get_action(s_c)
-                        tao_j.append(torch.cat((s_c, a_c), dim=1))
-
-                        # genreate new customer state
-                        a = self.EnginePolicy(s)
-                        s_c = torch.cat((s, a))
-
-                        page_index += 1
+                trajectory = self.G.generate(self.trajectory_num)
 
                 # sample gen trajectories
                 batch_gen_old = random.sample(trajectory, self.batch_size)
@@ -75,22 +45,20 @@ class MailModel:
 
                 # gradient ascent update Reward
                 for _ in range(1):
-                    self.optim_R.zero_grad()
+                    self.optim_D.zero_grad()
 
-                    expert_o = self.RewardModel(batch_expert.to(device))
-                    gen_o = self.RewardModel(batch_gen)
+                    expert_o = self.D(batch_expert.to(device))
+                    gen_o = self.D(batch_gen)
 
                     r_loss = self.loss_func(expert_o, torch.ones_like(expert_o, device=device)) + \
                              self.loss_func(gen_o, torch.zeros_like(gen_o, device=device))
                     r_loss.backward()
 
-                    self.optim_R.step()
+                    self.optim_D.step()
 
                 # TODO PPO update joint policy
 
-
     def save_model(self):
-        torch.save(self.UserPolicy.state_dict(), './model/UserPolicy.pt')
-        torch.save(self.EnginePolicy.state_dict(), './model/EnginePolicy.pt')
-        torch.save(self.UserLeaveModel.state_dict(), './model/UserLeaveModel.pt')
-        torch.save(self.RewardModel.state_dict(), '../model/RewardModel.pt')
+        torch.save(self.G.state_dict(), r'./model/policy.pt')
+        torch.save(self.V.state_dict(), r'./model/value.pt')
+        torch.save(self.D.state_dict(), r'./model/discriminator.pt')
---
+++ MAIL/MailPolicy.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created at 2019/12/5 下午2:59
+from GAN_SD.GeneratorModel import GeneratorModel
+from utils.utils import *
+
+
+class MailPolicy(nn.Module):
+    def __init__(self, activation=nn.LeakyReLU):
+        super(MailPolicy, self).__init__()
+
+        self.dim_user_state = 88 + 27 + 1
+        self.dim_user_action = 11
+
+        self.dim_engine_state = 88
+        self.dim_engine_hidden = 256
+        self.dim_engine_action = 27
+
+        self.dim_userleave_state = 88
+        self.dim_userleave_action = 101
+
+        self.UserModel = GeneratorModel()
+        self.UserModel.load()
+
+        self.EnginePolicy = nn.Sequential(
+            nn.Linear(self.dim_engine_state, self.dim_engine_hidden),
+            activation(),
+            nn.Linear(self.dim_engine_hidden, self.dim_engine_action),
+        )
+
+        self.UserPolicy = nn.Sequential(
+            nn.Linear(self.dim_user_state, 128),
+            activation(),
+            nn.Linear(128, 256),
+            activation(),
+            nn.Linear(256, self.dim_user_action)
+        )
+
+        self.UserLeavePolicy = nn.Sequential(
+            nn.Linear(self.dim_user_feature, 128),
+            nn.LeakyReLU(),
+            nn.Linear(128, 256),
+            nn.LeakyReLU(),
+            nn.Linear(256, self.dim_userleave_action)
+        )
+
+        self.UserPolicy.apply(init_weight)
+        self.EnginePolicy.apply(init_weight)
+        self.UserLeavePolicy.apply(init_weight)
+
+    def get_engine_action(self, engine_state):
+        return self.EnginePolicy(engine_state)
+
+    # user_state (user_feature, engine_action, page_index)
+    def get_user_action_prob(self, user_state):
+        action_prob = F.softmax(self.UserPolicy(user_state), dim=1)
+        return action_prob
+
+    def get_user_action(self, user_state):
+        action_prob = self.get_user_action_prob(user_state)
+        action = torch.multinomial(action_prob, 1)
+        return action
+
+    def get_user_leave_action(self, user):
+        x = self.UserLeavePolicy(user)
+        leave_page_index = torch.multinomial(F.softmax(x, dim=1), 1)
+        return leave_page_index
+
+    def generate_trajectory(self, trajectory_num):
+        # sample J trajectories
+        trajectory = []
+        for j in range(trajectory_num):
+            tao_j = []
+            # sample user from GAN-SD distribution
+            s, _ = self.UserModel.generate()
+            # get user's leave page index from leave model
+            leave_page_index = self.UserLeaveModel(s)
+            # get engine action from user with request
+            a = self.EnginePolicy(s)
+
+            s_c = torch.cat((s, a), dim=1)
+            page_index = 1
+
+            while page_index != leave_page_index:  # terminate condition
+                s_c = torch.cat((s_c, FLOAT(page_index)).to(device), dim=1)
+                a_c = self.UserPolicy.get_action(s_c)
+                tao_j.append(torch.cat((s_c, a_c), dim=1))
+
+                # genreate new customer state
+                a = self.EnginePolicy(s)
+                s_c = torch.cat((s, a))
+
+                page_index += 1
+
+    def get_log_prob(self, user_state, user_action):
+        pass
